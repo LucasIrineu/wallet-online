5ac3dc95c84d03c025cfadadebe1aa72e8fdd7fb
Lucas Irineu <lucasirineu119@gmail.com>
2022-09-02 20:41:35 -0300

    requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 
 class Table extends Component {
   render() {
+    const tableHeaders = ['Descrição', 'Tag', 'Método de pagamento',
+      'Valor', 'Moeda', 'Câmbio utilizado',
+      'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              { tableHeaders.map((element) => (
+                <th key={ element }>
+                  {element}
+                </th>
+              ))}
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
